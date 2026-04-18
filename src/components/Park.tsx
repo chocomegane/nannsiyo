47ce21830c6eb1224c7358fd1cd5ff33cceba44d
hatuka <ukymegane@gmail.com>
2026-04-18 20:04:05 +0900

    UI変更

--- src/components/Park.tsx
+++ src/components/Park.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { parkSocket } from '../lib/socket'
 import { useWorldStore } from '../store/worldStore'
